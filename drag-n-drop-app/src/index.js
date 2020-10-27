29bfe8ff724cb345c48b79837e505d1c14fea7f0
Linda Nguyen <lnguyen@ss8.com>
2020-10-27 09:28:10 -0700

    added feature to drag and drop images

--- drag-n-drop-app/src/index.js
+++ drag-n-drop-app/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './index.css';
 import { render } from 'react-dom';
 import { Navbar } from 'react-bootstrap';
 import { Stage, Layer, Image } from 'react-konva';
@@ -8,8 +9,21 @@ import cherry from './cherry.png';
 import coconut from './coconut.png';
 import peach from './peach.png';
 import pear from './pear.png';
-// import useImage from 'use-image'
+import useImage from 'use-image'
 
+const URLImage = ({ image }) => {
+  const [img] = useImage(image.src);
+  return (
+    <Image
+      image={img}
+      x={image.x}
+      y={image.y}
+      // I will use offset to set origin to the center of the image
+      offsetX={img ? img.width / 2 : 0}
+      offsetY={img ? img.height / 2 : 0}
+    />
+  );
+};
 
 const App = () => {
   const dragUrl = React.useRef(); 
@@ -26,8 +40,8 @@ const App = () => {
       <img
           alt="apple"
           src={ apple }
-          width="160"
-          height="160"
+          width="130"
+          height="130"
           draggable="true"
           onDragStart={e => {
             dragUrl.current = e.target.src; 
@@ -36,8 +50,8 @@ const App = () => {
         <img
           alt="orange"
           src={ orange }
-          width="160"
-          height="160"
+          width="130"
+          height="130"
           draggable="true"
           onDragStart={e => {
             dragUrl.current = e.target.src; 
@@ -47,8 +61,8 @@ const App = () => {
         <img
           alt="cherry"
           src={ cherry }
-          width="160"
-          height="160"
+          width="130"
+          height="130"
           draggable="true"
           onDragStart={e => {
             dragUrl.current = e.target.src; 
@@ -58,8 +72,8 @@ const App = () => {
         <img
           alt="pear"
           src={ pear }
-          width="160"
-          height="160"
+          width="130"
+          height="130"
           draggable="true"
           onDragStart={e => {
             dragUrl.current = e.target.src; 
@@ -69,8 +83,8 @@ const App = () => {
       <img
         alt="peach"
         src={ peach }
-        width="160"
-        height="160"
+        width="130"
+        height="130"
         draggable="true"
         onDragStart={e => {
           dragUrl.current = e.target.src; 
@@ -80,16 +94,47 @@ const App = () => {
         <img
           alt="coconut"
           src={ coconut }
-          width="160"
-          height="160"
+          width="130"
+          height="130"
           draggable="true"
           onDragStart={e => {
             dragUrl.current = e.target.src; 
         }}
         />
-    </div>
+    
+    
+    <div
+      onDrop = { e => {
+        // register event position
+        stageRef.current.setPointersPositions(e); 
+        // add image 
+        setImages(
+          images.concat([
+            {
+              ...stageRef.current.getPointerPosition(),
+              src: dragUrl.current
+            }
+          ])
+        );
+      }}
+      onDragOver = { e => e.preventDefault()}
+      >
+        <Stage
+          width = { window.innerWidth }
+          height = { window.innerHeight }
+          style = {{ border: '2px solid grey' }}
+          ref = { stageRef }
+          >
+            <Layer>
+              { images.map(image => {
+                return <URLImage image = { image } />;
+              })}
+            </Layer>
+          </Stage>
+      </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 render(<App />, document.getElementById('root'));
